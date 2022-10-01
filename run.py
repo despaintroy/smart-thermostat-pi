e0b8ad699adc80c691dcd58a2113310913ac13b5
despaintroy <despaintroy@gmail.com>
2022-10-01 14:52:45 -0600

    Control relay

--- run.py
+++ run.py
@@ -1,13 +1,29 @@
 import adafruit_dht
-from board import D4
+import board
 from time import sleep, time
+import RPi.GPIO as GPIO
 
-dht_device = adafruit_dht.DHT22(D4)
+FURNACE_PIN_NUMBER = 21
+SENSOR_PIN = board.D4
+
+# Setup relay for controlling furnace
+GPIO.setmode(GPIO.BCM)  # GPIO Numbers instead of board numbers
+GPIO.setup(FURNACE_PIN_NUMBER, GPIO.OUT)  # GPIO Assign mode
+
+# Setup temp/humidity sensor
+dht22 = adafruit_dht.DHT22(SENSOR_PIN, False)
+
+
+def set_furnace(value):
+    GPIO.output(FURNACE_PIN_NUMBER, GPIO.HIGH if value else GPIO.LOW)
+
 
 while True:
     try:
-        temperature = dht_device.temperature
-        humidity = dht_device.humidity
+        temperature = dht22.temperature
+        humidity = dht22.humidity
+
+        set_furnace(humidity > 50.0)
 
         print('Temp={0:0.1f}ºC  Humidity={1:0.1f}%'.format(
             temperature, humidity))
@@ -18,10 +34,10 @@ while True:
         continue
 
     except Exception as error:
-        dht_device.exit()
+        dht22.exit()
         raise error
 
-    sleep(5)
+    sleep(3)
 
 
 # import firebase_admin
