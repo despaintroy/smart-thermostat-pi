f3e7a596bd5db6e9a54ca74bd2cb16dd49f5b730
Troy DeSpain <despaintroy@gmail.com>
2022-10-04 21:01:58 -0600

    Use correct pin for furnace

--- src/hardware.ts
+++ src/hardware.ts
@@ -1,5 +1,10 @@
 import { promises } from 'node-dht-sensor'
-import { MOCK_ENVIRONMENT, SENSOR_PIN, SENSOR_TYPE } from './environment'
+import {
+	FURNACE_PIN,
+	MOCK_ENVIRONMENT,
+	SENSOR_PIN,
+	SENSOR_TYPE,
+} from './environment'
 import { Gpio } from 'onoff'
 import { MockGpio } from './mockGpio'
 
@@ -20,7 +25,9 @@ if (MOCK_ENVIRONMENT) {
 	initialize(SENSOR_TYPE, SENSOR_PIN)
 }
 
-const furnace = MOCK_ENVIRONMENT ? new MockGpio(4, 'out') : new Gpio(4, 'out')
+const furnace = MOCK_ENVIRONMENT
+	? new MockGpio(FURNACE_PIN, 'out')
+	: new Gpio(FURNACE_PIN, 'out')
 
 export const measureTempHumid = () => read(SENSOR_TYPE, SENSOR_PIN)
 
